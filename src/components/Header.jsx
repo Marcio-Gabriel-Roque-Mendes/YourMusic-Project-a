bb2b6055d0e02d059861821724d335031167e94a
Marcio Gabriel Roque Mendes <marcio_2411@hotmail.com>
2022-05-19 11:36:31 -0300

    correcao req 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+
+class Header extends Component {
+  render() {
+    return (
+      <header data-testid="header-component">
+        Alguma coisa
+      </header>
+    );
+  }
+}
+export default Header;
--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class ProfileEdit extends Component {
   render() {
     return (
       <div data-testid="page-profile-edit">
-        Alguma coisa
+        <Header />
+        <p>profile edit</p>
       </div>
     );
   }
