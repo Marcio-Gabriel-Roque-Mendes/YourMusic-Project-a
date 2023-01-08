0be499a7f238f5844faabdd31f6205a3f59e0a75
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2023-01-08 15:02:27 -0300

    Feat: Adição de titulo e classNames de estização

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-state */
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
@@ -17,26 +18,59 @@ class Header extends Component {
   }
 
   render() {
-    const { user, loading } = this.state;
+    const { /* user */ loading } = this.state;
 
     if (loading) {
-      return <div>Carregando...</div>;
+      return <div className="text-indigo-800">Carregando...</div>;
     }
     return (
-      <header data-testid="header-component">
-        <nav>
-          {/* <Link to="/">Home </Link> */}
-          <Link to="/profile" data-testid="link-to-profile">User </Link>
-          {' '}
-          <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
-          {' '}
-          <Link to="/favorites" data-testid="link-to-favorites">Musicas Favoritas </Link>
-        </nav>
-        <p data-testid="header-user-name">
-          {' '}
-          {user}
-        </p>
-      </header>
+      <>
+        <header
+          data-testid="header-component"
+          className="flex flex-row justify-between items-center py-2.5 px-5 bg-gray-300"
+        >
+          <h1 className="text-6xl my-4 text-purple-800">
+            {/* <img src={imageMusica} alt="Logo de nota musical" />  */}
+            YourMusic
+          </h1>
+          <nav className="text-indigo-900 ">
+            {/* <Link to="/">Home </Link> */}
+            <Link
+              to="/profile"
+              data-testid="link-to-profile"
+              className="mx-2.5 hover:text-indigo-500 hover:font-bold"
+            >
+              User
+            </Link>
+            {' '}
+            <Link
+              to="/search"
+              data-testid="link-to-search"
+              className="mx-2.5 hover:text-indigo-500 hover:font-bold"
+            >
+              Pesquisar
+            </Link>
+            {' '}
+            <Link
+              to="/favorites"
+              data-testid="link-to-favorites"
+              className="mx-2.5 hover:text-indigo-500 hover:font-bold"
+            >
+              Musicas Favoritas
+              {' '}
+
+            </Link>
+          </nav>
+
+        </header>
+        <main className="bg-violet-100">
+          {/* <p data-testid="header-user-name" className='my-2.5 text-indigo-800 bg-violet-100'>
+       {' '}
+       {`Boas-vindas, ${user}`}
+     </p> */}
+        </main>
+
+      </>
     );
   }
 }
